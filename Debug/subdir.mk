7124e9fe92e895f0bd2f55cf607852d0a127d0ba
trejonh <houset@msoe.edu>
2017-05-11 09:22:30 -0500

    build

---
+++ Debug/subdir.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../AudioInterface.cpp \
+../Client.cpp \
+../ClientAudio.cpp 
+
+OBJS += \
+./AudioInterface.o \
+./Client.o \
+./ClientAudio.o 
+
+CPP_DEPS += \
+./AudioInterface.d \
+./Client.d \
+./ClientAudio.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross G++ Compiler'
+	arm-linux-gnueabihf-g++ -D__GXX_EXPERIMENTAL_CXX0X__ -D__cplusplus=201103L -O0 -g3 -Wall -c -fmessage-length=0 -std=c++11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
