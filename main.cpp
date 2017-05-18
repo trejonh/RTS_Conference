2cc34cff7e27f5efafc7dbac9e67f864ca07e280
trejonh <houset@msoe.edu>
2017-05-17 23:11:08 -0500

    update

--- main.cpp
+++ main.cpp
@@ -12,16 +12,17 @@
 using namespace std;
 
 int main(int argc, char** argv){
-	if(argc != 3){
+	if(argc != 4){
+		cout << "Usage: <AppName> <Audio Recorder Hardware> <Audio Port> <Video Port>" << endl;
 		exit(-1);
 	}
 	Client client(argv[1],argv[2],80,90,20);
 	client.Stream();
-	cout << "enter quit to stop stream"<<endl;
-	char* input;
+	cout << "enter 0 to stop stream"<<endl;
+	int input = -1;
 	while(1){
 		cin >> input;
-		if(strcmp(input,"quit")==0)
+		if(input==0)
 			return 0;
 	}
 	return 0;
